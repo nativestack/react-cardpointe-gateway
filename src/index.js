94e54763de2dc98ea048e5eeb2b182ab34aff326
LopezDP <davidplopez@live.com>
2020-05-08 22:05:51 -0400

    refactor: updating gateway package to work with older react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,71 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import NativeStackTokenizer from "./NativeStackTokenizer";
-import * as serviceWorker from "./serviceWorker";
+import React, { Component } from "react";
+import "./NativeStackTokenizer.css";
+import CardPointeTokenizer from "./components/CardPointeTokenizer";
 
-ReactDOM.render(<NativeStackTokenizer />, document.getElementById("root"));
+class NativeStackTokenizer extends Component {
+  constructor(props) {
+    super(props);
+    // NOTE: Parent must have emvData object
+    this.state = {
+      emvData: "",
+    };
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  componentDidUpdate() {
+    console.log("This is the TokenData: ", this.state.emvData);
+    /* NOTE: This is the function passed into props
+     *
+     *       This will send token data back to Parent component
+     *
+     *       @return {token: "9418594164541111", expiryDate: "202312"}
+     */
+    try {
+      this.props.tokenProps.userEmvData(this.state.emvData);
+    } catch (err) {
+      console.log("UPDATING CARD");
+    }
+  }
+
+  /* NOTE:
+   * @function userEmvData
+   * @param emvData
+   * Parent component must implement function
+   * to pass emvData returned from child into
+   * state.
+   */
+  userEmvData = (emvData) => {
+    this.setState({
+      emvData: emvData,
+    });
+  };
+
+  render() {
+    /* NOTE:
+     * @const tokenProps
+     * Parent component must declare tokenProps
+     * in render function to pass userEmvData
+     * function into child component props.
+     */
+    const tokenProps = {
+      // below is token info
+      userEmvData: this.userEmvData,
+    };
+
+    /*
+     * NOTE: User has to pass tokenProps into props
+     *       for child component to allow this to access
+     *       the function in parent component.
+     */
+    return (
+      <div className="App">
+        <CardPointeTokenizer
+          site={this.props.site}
+          port={this.props.port}
+          tokenProps={tokenProps}
+        />
+      </div>
+    );
+  }
+}
+
+export default NativeStackTokenizer;
